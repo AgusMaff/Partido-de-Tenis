6ea507c28d7a23c3a0703d7fb3df74b66a85aae3
AgusMaff <102746223+AgusMaff@users.noreply.github.com>
2022-12-28 12:29:37 -0300

    Delete Tenis directory

--- Tenis/src/Observable.java
+++
@@ -1,7 +0,0 @@
-public interface Observable {
-    public void notificar();
-    public void anadirObservador(Observer o);
-
-    public void removerObservador(Observer o);
-
-}
--- Tenis/src/Observer.java
+++
@@ -1,5 +0,0 @@
-import java.util.ArrayList;
-
-public interface Observer {
-    public void update(ArrayList<Jugador> j, ArrayList<Sets> s);
-}
